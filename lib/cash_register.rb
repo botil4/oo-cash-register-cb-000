3d9332a10f4dc0e2e473d37cc9f196ff34f16ce6
Travis Clarkson <tk1667@gmail.com>
2018-02-24 21:02:05 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,7 +6,7 @@ class CashRegister
 
   def initialize(discount = 0)
     @total = 0
-    @discount = discount.to_f/100
+    @discount = discount.to_i
   end
 
   def add_item(item, price, qty = 1)
@@ -16,7 +16,7 @@ class CashRegister
 
   def apply_discount
     if @discount > 0
-      @total *= 1-@discount
+      @total *= 1-(@discount/100.)
       msg = "After the discount, the total comes to $#{@total}."
     else
       error_message =  "There is no discount to apply."

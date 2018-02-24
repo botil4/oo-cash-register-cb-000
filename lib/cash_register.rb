7ba5d8ba3c3c6f405f093b3f4b22e5a1a24d5495
Travis Clarkson <tk1667@gmail.com>
2018-02-24 20:29:37 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,8 +15,11 @@ class CashRegister
   end
 
   def apply_discount
+    if @discount > 0
     @total *= @discount/100
     puts "After the discount, the total comes to $#{@total}."
+  else
+    puts "There is no discount to apply"
   end
 
 end
